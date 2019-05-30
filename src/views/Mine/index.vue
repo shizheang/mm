87c807b668afaa95a9c5182b7633e4943e9a99f6
shizheang <51151387+shizheang@users.noreply.github.com>
2019-05-30 19:39:05 +0800

    teeeee

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div  id="main" >
+		<Header title="我的喵喵电影"/>
+		<TabBar/>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/Header';
+	import TabBar from '@/components/TabBar';
+	export default{
+		name:'Mine',
+		components:{
+			Header,TabBar,
+		}
+	}
+</script>
+
+<style>
+</style>
